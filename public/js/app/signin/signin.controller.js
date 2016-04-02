ffe699c7f6b040906998dd69d6d8dcec77449a3d
Jerry Ngov <jcngov@yahoo.com>
2016-04-02 15:27:47 -0700

    succesfully created user using sign up form

--- public/js/app/signin/signin.controller.js
+++ public/js/app/signin/signin.controller.js
@@ -5,13 +5,49 @@
     .module('app')
     .controller('SignInController', SignInController);
 
-  SignInController.$inject = ['$log'];
+  SignInController.$inject = ['$log', '$http'];
 
-  function SignInController($log) {
+  function SignInController($log, $http) {
 
-    var vm = this;
     $log.info('SignInController loaded');
 
+    var vm = this;
+    vm.signUp = signUp;
+    vm.users = [];
+    vm.user = {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      passwordConfirmation: ""
+    };
+
+
+    function signUp(user) {
+      $log.info(user);
+      $http({
+        method: 'POST',
+        url: 'http://localhost:3000/api/users',
+        data: {
+          firstName:            vm.user.firstName,
+          lastName:             vm.user.lastName,
+          email:                vm.user.email,
+          password:             vm.user.password,
+          passwordConfirmation: vm.user.passwordConfirmation
+        },
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(
+        function(res) {
+          $log.info("Success ", res)
+        },
+        function(err){
+          $log.info("Error: ", err)
+        }
+      );
+    };
   }
 
 })();

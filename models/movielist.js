b64f5689d1ea37e3bd3a62c74811db0cf223200b
Jerry Ngov <jcngov@yahoo.com>
2016-04-01 23:32:07 -0700

    create user model and movielist model

---
+++ models/movielist.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose'),
+    debug    = require('debug')('app:models'),
+
+var movieSchema = new mongoose.Schema({
+  title:         {type: String},
+  tagline:       {type: String},
+  overview:      {type: String},
+  poster_path:   {type: String},
+  backdrop_path: {type: String},
+  release_date:  {type: Date},
+  genre_ids:     {type: Number},
+  popularity:    {type: Number}
+});
+
+var favoritesSchema = new mongoose.Schema({
+  movies: [movieSchema],
+  creator:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+          }
+});
+
+var movieListSchema = new mongoose.Schema({
+  movies:          [movieSchema],
+  favorite_movies: [favoritesSchema]
+});
+
+var Movielist = mongoose.model('Movielist', movieListSchema);
+
+module.exports = Movielist;
+
+
--- models/user.js
+++ models/user.js
@@ -2,10 +2,15 @@ var mongoose = require('mongoose'),
     debug    = require('debug')('app:models');
 
 var userSchema = new mongoose.Schema({
-  name:   String,
-  handle: String
+  firstName:            {type: String, required: true},
+  lastName:             {type: String, required: true},
+  email:                {type: String, required: true, unique: true},
+  password:             {type: String, required: true},
+  passwordConfirmation: {type: String, required: true}
 });
 
 var User = mongoose.model('User', userSchema);
 
 module.exports = User;
+
+// how to model users following users

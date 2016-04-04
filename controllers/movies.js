39e9971cc2f59702edb4ff1c789fb0fb9dcfeb17
Jerry Ngov <jcngov@yahoo.com>
2016-04-03 23:33:35 -0700

    npm install dotenv, and play around with movie api

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,7 +1,20 @@
 var request = require('request');
+var dotenv  = require('dotenv').config();
 
 var uri =  'https://api.themoviedb.org/3/';
+var key = process.env.API_KEY;
 
-function search(req, res,next) {
-
+function search(req, res, next) {
+  request({
+    method: 'GET',
+    uri: uri + 'movie/550?api_key=' + key
+  }, function (err, res, body) {
+    if (!err) {
+      var movie = JSON.parse(body);
+      console.log(movie);
+    }
+  });
 }
+
+search();
+
